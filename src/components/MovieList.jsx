6bf58640bb84b0f82bc00299bbab70aad931c8d5
David smart <112566573+davidgraphix@users.noreply.github.com>
2025-01-08 14:34:33 +0100

    Update Movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
 export const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
   return (
     <>
-      {props.movies.map((movie, index) => (
-        <div key={movie.imdbID}>
-          <img src={movie.Poster} alt="movie"></img>
-        </div>
-      ))}
+      {props.movies && props.movies.length > 0 ? (
+        props.movies.map((movie, index) => (
+          <div
+            key={movie.imdbID}
+            className="image-container d-flex justify-content-start m-3"
+          >
+            <a
+              href={`https://www.imdb.com/title/${movie.imdbID}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={movie.Poster} alt="movie"></img>
+            </a>
+            <div
+              onClick={() => props.handleFavouritesClick(movie)}
+              className="overlay d-flex align-items-center justify-content-center"
+            >
+              <FavouriteComponent />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No movies found</p>
+      )}
     </>
   );
 };
